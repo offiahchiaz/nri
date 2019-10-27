21e60c57bc46544602189edfc37cf06182bff595
offiahchiaz <offiahchiaz@gmail.com>
2019-10-27 21:47:12 +0100

    UI refactoring

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title> {{config('app.name', 'Nri :: A Recipe App')}} </title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
     </head>
     <body>
         @yield('content')
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,5 @@
-<h1>About Page</h1>
+@extends('layouts.app')
+
+@section('content')
+    <h1>About Page</h1>
+@endsection
