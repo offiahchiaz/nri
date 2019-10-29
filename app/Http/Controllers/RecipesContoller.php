b9e85a74150c7f920edf0490c44040d9a5fb511f
offiahchiaz <offiahchiaz@gmail.com>
2019-10-29 23:53:57 +0100

    Refactored app from using menu to using recipe

--- app/Http/Controllers/RecipesContoller.php
+++ app/Http/Controllers/RecipesContoller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MenusController extends Controller
+class RecipesContoller extends Controller
 {
     /**
      * Display a listing of the resource.
--- database/migrations/2019_10_29_223726_create_recipes_table.php
+++ database/migrations/2019_10_29_223726_create_recipes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenusTable extends Migration
+class CreateRecipesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateMenusTable extends Migration
      */
     public function up()
     {
-        Schema::create('menus', function (Blueprint $table) {
+        Schema::create('recipes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
@@ -29,6 +29,6 @@ class CreateMenusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('menus');
+        Schema::dropIfExists('recipes');
     }
 }
